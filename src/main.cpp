db715e4c7d2d723ff0869a48e0ef5b5ea98d56ef
jahnhtl <philipp.jahn@htlwien10.at>
2026-02-16 16:15:41 +0100

    Usage of Timer2 instead of Timer0

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,8 @@ void setup()
   // Pin PB5 als Ausgang konfigurieren
   DDRB = DDRB | (1 << PB5);
 
-  // Timer0 konfigurieren mit Prescaler 1024
-  TCCR0B = (1 << CS02) | (1 << CS00);
+  // Timer2 konfigurieren mit Prescaler 1024
+  TCCR2B = (1 << CS22) | (1 << CS21) |(1 << CS20);
 
 }
 
@@ -15,10 +15,10 @@ void loop()
   static uint8_t overflows_anzahl = 0;
 
   // Check ob Overflow von Timer0 aufgetreten ist
-  if(TIFR0 & (1 << TOV0))
+  if(TIFR2 & (1 << TOV2))
   {
     overflows_anzahl++;
-    TIFR0 = TIFR0 | (1 << TOV0);
+    TIFR2 = TIFR2 | (1 << TOV2);
   }
 
   if(overflows_anzahl >= 32)
